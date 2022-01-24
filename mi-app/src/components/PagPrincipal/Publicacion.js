a4922e80f1e2fc5d97e0863c1d3bc1d5622be530
CristopherO <cristopher_ob@hotmail.com>
2022-01-24 16:38:14 -0600

    CSS (corrección de input y colores de login), Navbar (se añadió opción de ir a perfil)

--- mi-app/src/components/PagPrincipal/Publicacion.js
+++ mi-app/src/components/PagPrincipal/Publicacion.js
@@ -1,30 +1,3 @@
-/* import React from "react";
-export default function Publicacion(){
-    return(
-        <div className ="col-12 col-md-8 col-xl-8 contenido ">
-                    <div className ="publicacion">
-                        <div className ="card text-center">
-                            <div className ="card-header d-flex">
-                              <ul className ="nav nav-pills card-header-pills">
-                                <li className ="nav-item">
-                                  <a className ="card-image foto-perfil" href="#"><img src="../img/perfil_2.jpg" alt="foto" className ="border 
-                                    border-secondary  border-3 rounded-circle"/></a>Que estas pensando el dia de hoy <span id="nombre"></span>?
-                                </li>
-                               
-                              </ul>
-                            </div>
-                            <div className ="card-body">
-                              <h5 className ="card-title nueva_publicacion">Nueva publicación</h5>
-        
-                              <a href="#" className ="btn ">Publicar</a>
-                            </div>
-                          </div>
-                    </div>
-        </div>
-    )
-} */
-
-
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
@@ -38,19 +11,19 @@ function Publicacion({ texto_publicacion }) {
 
   useEffect(() => {
 
-    if (Object.keys(setTexto).length > 0) { setTexto(texto_publicacion.texto)}
+    if (Object.keys(setTexto).length > 0) { setTexto(texto_publicacion.texto) }
 
-  },[texto_publicacion])
+  }, [texto_publicacion])
 
   const [mostrar, setMostrar] = useState(true)
 
-  const handleSubmit =(e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     setMostrar(false);
-   
-    
+
+
     dispatch(logout())
-    if([texto].includes("")){
+    if ([texto].includes("")) {
 
       window.alert("No puedes publicar un post en blanco");
 
@@ -72,71 +45,53 @@ function Publicacion({ texto_publicacion }) {
 
   }*/
 
-    const cuerpoPublicacion = {texto, /*id:generarId()*/}
+    const cuerpoPublicacion = { texto, /*id:generarId()*/ }
 
-   /* if(cuerpoPublicacion.id){
+    /* if(cuerpoPublicacion.id){
+ 
+       cuerpoPublicacion.id = texto_publicacion.id
+ 
+       const actualizacionPublicación = texto_publicacion.map(publicacion => (publicacion.id===texto_publicacion.id ? cuerpoPublicacion : publicacion))
+ 
+       setPublicacion(cuerpoPublicacion)
+ 
+       setPublicacion({})
+ 
+     } else {
+ 
+       cuerpoPublicacion.id=generarId();
+ 
+       setPublicacion([...texto_publicacion, cuerpoPublicacion])
+ 
+     }
+ 
+     setTexto("");*/
 
-      cuerpoPublicacion.id = texto_publicacion.id
-
-      const actualizacionPublicación = texto_publicacion.map(publicacion => (publicacion.id===texto_publicacion.id ? cuerpoPublicacion : publicacion))
-
-      setPublicacion(cuerpoPublicacion)
-
-      setPublicacion({})
-
-    } else {
-
-      cuerpoPublicacion.id=generarId();
-
-      setPublicacion([...texto_publicacion, cuerpoPublicacion])
-
-    }
-
-    setTexto("");*/
-
-}
+  }
 
   return (
-
     <div className="col-12 col-md-8 col-xl-8 contenido ">
-
       <div className="publicacion">
-
         <div className="card text-center">
-
           <div className="card-header d-flex">
-
             <ul className="nav nav-pills card-header-pills">
-
               <li className="nav-item">
-
                 <a className="card-image foto-perfil" href="#"><img src="../img/perfil_2.jpg" alt="foto" className="border 
-
                                     border-secondary  border-3 rounded-circle"/></a>Que estas pensando el dia de hoy <span id="nombre"></span>?
-
               </li>
-
             </ul>
-
           </div>
-
           <div className="card-body">
-
             <form className="form new_pub" type="POST" onSubmit={handleSubmit} >
-
-              <input type="text" className="pub_body" id="txtPublicacion" value={texto} onChange={(e)=> setTexto(e.target.value)}></input>
-
+              <input type="text" className="pub_body" id="txtPublicacion" value={texto} onChange={(e) => setTexto(e.target.value)}></input>
               <button type="submit" className="btn btn-primary mb-2">Publicar</button>
-
             </form>
-
           </div>
-
         </div>
-
       </div>
-
     </div>
+
+    
 
   )
 
