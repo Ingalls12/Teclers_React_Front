ebd7f9524e01a3446b31b29463961c318ed06fda
Ingalls12 <wertyun91@gmail.com>
2022-01-26 17:13:55 -0600

    borre los contenidos de amigos recientes y buscar amigos

--- mi-app/src/components/AgregarAmigos/AmigosRecientes.js
+++ mi-app/src/components/AgregarAmigos/AmigosRecientes.js
@@ -5,22 +5,6 @@ export default function AmigosRecientes(){
             <div className ="amigos_recientes">
                 <h2>Amigos Recientes</h2>
                 <hr/>
-                <a href="" className ="perfil">
-                    <img src="../img/perfil.jpg" alt=""/>
-                    <h3>Amigo</h3>
-                </a>
-                <a href="" className ="perfil">
-                    <img src="../img/perfil.jpg" alt=""/>
-                    <h3>Amigo</h3>
-                </a>
-                <a href="" className ="perfil">
-                    <img src="../img/perfil.jpg" alt=""/>
-                    <h3>Amigo</h3>
-                </a>
-                <a href="" className ="perfil">
-                    <img src="../img/perfil.jpg" alt=""/>
-                    <h3>Amigo</h3>
-                </a>
             </div>
         </div>
     )
